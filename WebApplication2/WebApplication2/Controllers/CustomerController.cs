864fe0ede141afad0613fd3137f41455b368f6dc
mooCorp-1 <68877392+mooCorp-1@users.noreply.github.com>
2020-07-27 16:11:35 -0600

    Add files via upload

---
+++ WebApplication2/WebApplication2/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ViewDetails()
+        {
+            CustomerDataModel cd = new CustomerDataModel();
+            cd.Name = "Steven Clark";
+            cd.Age = 34;
+            ViewBag.Name = cd.Name;
+            ViewBag.Age = Convert.ToString(cd.Age);
+            return View("Index");
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/StevenClarkController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class StevenClarkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult YourMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PrintMessage()
+        {
+            string msg = HttpContext.Request.Form["Text1"].ToString();
+            ViewBag.Messages = msg.ToString();
+            return View("YourMessage");
+        }
+    }
+}
